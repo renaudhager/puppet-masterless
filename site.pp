264fd5680ebd9eafcd375a9e32f29d79e95f7409
Renaud Hager <rhager@rhager-mbp.local>
2016-09-15 20:12:52 +0100

    First commit

---
+++ site.pp
@@ -0,0 +1,22 @@
+
+
+Exec {
+  path => '/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin',
+}
+
+# Regex on hostname to determine the role of the node
+if empty( hiera( 'role', undef ) )
+{
+  case $::hostname {
+    /compute-sl\d\d$/:  { $role = 'mesos-slave' }
+    /compute-ma\d\d$/:  { $role = 'mesos-master' }
+    default:            { $role = undef }
+  }
+}
+else {
+  $role = hiera( 'role' )
+}
+
+node default {
+  hiera_hash('include')['classes'].each |$c| { if $c !~ /^--/ and ! defined( Class[$c] ) { include $c } }
+}
