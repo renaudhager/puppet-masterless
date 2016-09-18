2ad065b2ede26a5d492b8f70a4ef57d2cd190c8d
Renaud Hager <renaud.hager@gmail.com>
2016-09-18 10:51:50 +0200

    Add dnsmasq. Update consul configuration. Add a manifest to handle basic resources.

---
+++ site/profiles/manifests/system/dnsmasq.pp
@@ -0,0 +1,35 @@
+# profiles::system::dnsmasq
+#
+# This class manages DNSMASQ configuration on nodes
+#
+# Required modules :
+# - saz/dnsmasq
+#
+class profiles::system::dnsmasq (
+
+)  {
+  # Deep Merge bug
+  $dnsmasq_conf = hiera_hash( 'profiles::system::dnsmasq::conf', {} )
+
+  $defaults = {
+    ensure => present,
+  }
+
+  create_resources( dnsmasq::conf, $dnsmasq_conf, $defaults )
+
+  file { '/etc/default/dnsmasq':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => file( 'profiles/system/dnsmasq/dnsmasq.default' ),
+    notify  => Service['dnsmasq'],
+  }
+
+  file { '/var/run/dnsmasq/resolv.conf':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => ('profiles/system/dnsmasq/resol.conf')
+  }
+
+}
