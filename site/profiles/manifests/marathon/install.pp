852b769bbb01c36fee0a75cca5947cd4b33bb7db
Renaud Hager <renaud.hager@gmail.com>
2016-09-25 23:08:45 +0100

    Update ZK config in marathon.

--- site/profiles/manifests/marathon/install.pp
+++ site/profiles/manifests/marathon/install.pp
@@ -45,11 +45,13 @@ class profiles::marathon::install (
 
   }
 
+  # TODO : Need to put theses var in hiera
   class { '::marathon':
     package_ensure => 'present',
     manage_repo    => false,
     install_java   => false,
     extra_options  => '--task_launch_timeout 600000',
+    zk             => [ 'compute-ma01.node.vgt.consul:2181', 'compute-ma02.node.vgt.consul:2181', 'compute-ma03.node.vgt.consul:2181' ],
     require        => [ Exec['configure_java'], File['/etc/marathon/conf/hostname'] ],
   }
 }
