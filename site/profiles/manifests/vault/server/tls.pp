ad9f47f7cedcf3977b6d45b9c9aae60663d835b9
Renaud Hager <renaud.hager@gmail.com>
2016-10-17 22:55:52 +0100

    Add TLS cert for vault nodes.

---
+++ site/profiles/manifests/vault/server/tls.pp
@@ -0,0 +1,47 @@
+# profiles::vault::server::tls
+#
+# This class manages cert/key file for vault servers nodes.
+#
+
+class profiles::vault::server::tls (
+  String $encrypted_key_file = 'profiles/vault/server/tls/key.pem.enc',
+  String $encrypted_key_file_path = '/etc/vault/ssl/key.pem.enc',
+  String $tls_key_file_path = '/etc/vault/ssl/key.pem',
+  String $cert_file = 'profiles/vault/server/tls/cert.pem',
+  String $cert_file_path = '/etc/vault/ssl/cert.pem',
+  String $encrytion_key_name = 'cert',
+  String $vault_token_id = 'decrypt_cert',
+  ) {
+
+  file { '/etc/vault/ssl':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    require => Class['::vault'],
+  }
+
+  file { $cert_file_path:
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => file( $cert_file ),
+    require => File['/etc/vault/ssl'],
+  }
+
+  file { $encrypted_key_file_path:
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => file( $encrypted_key_file ),
+    require => File['/etc/vault/ssl'],
+    notify  => Exec['decrypt_tls_key'],
+  }
+
+  exec { 'decrypt_tls_key':
+    command => "/usr/local/bin/vault-decrypt.bash -f ${encrypted_key_file_path} -k ${encrytion_key_name} -o ${tls_key_file_path} -ti ${vault_token_id}",
+    unless  => "ls ${tls_key_file_path}",
+    require => File[$encrypted_key_file_path],
+  }
+
+}
