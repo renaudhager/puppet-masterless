ed874333b06f283bea864093f14468b5b6d99df7
Renaud Hager <renaud.hager@gmail.com>
2016-10-18 17:29:34 +0100

    Revamp datacenter assignation in site.pp.

--- site.pp
+++ site.pp
@@ -8,17 +8,37 @@ Exec {
 if empty( hiera( 'role', undef ) )
 {
   case $::hostname {
-    /compute-sl\d\d$/:  { $role = 'mesos-slave' }
-    /compute-ma\d\d$/:  { $role = 'mesos-master' }
-    /util-puppetdb\d\d$/:  { $role = 'puppetdb' }
-    default:            { $role = undef }
+    /compute-sl\d\d$/:    { $role = 'mesos-slave' }
+    /compute-ma\d\d$/:    { $role = 'mesos-master' }
+    /util-puppetdb\d\d$/: { $role = 'puppetdb' }
+    default:              { $role = undef }
   }
 }
 else {
   $role = hiera( 'role' )
 }
 
-$datacenter = hiera( 'datacenter' )
+# Assign DC
+# if eth0 is 10.0.2.15 DC => vgt
+# else use the regexp
+if empty( hiera( 'datacenter', undef ) )
+{
+  if $::ipaddress_eth0 == '10.0.2.15' {
+    $datacenter = 'vgt'
+  }
+  else {
+    # Split hostname to find DC
+    $hostname_infos = split( $::hostname, '-' )
+    case $hostname_infos[0] {
+      'ew1':   { $datacenter = 'ew1' }
+      default: { $datacenter = undef }
+    }
+  }
+}
+else {
+  $datacenter = hiera( 'datacenter' )
+}
+
 
 node default {
   hiera_hash('include')['classes'].each |$c| { if $c !~ /^--/ and ! defined( Class[$c] ) { include $c } }
